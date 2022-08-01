ee8784bfc14521487b576de88fcd0b35baa5088c
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-01 14:02:33 +0200

    dashboard page and controller class + messages

--- src/main/java/se/lexicon/springbootthymeleafproject/Controller/DashboardController.java
+++ src/main/java/se/lexicon/springbootthymeleafproject/Controller/DashboardController.java
@@ -2,13 +2,23 @@ package se.lexicon.springbootthymeleafproject.Controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class DashboardController {
 
     @GetMapping("/dashboard")
-    public String dashboard(){
+    public String dashboard(Model model){
+        LocalDate currentDate = LocalDate.now();
+        model.addAttribute("currentDate",currentDate);
+
+        model.addAttribute("productsListSize", 2);
+        model.addAttribute("categoryListSize", 11);
+        model.addAttribute("userListSize", 5);
+
         return "dashboard";
     }
 
