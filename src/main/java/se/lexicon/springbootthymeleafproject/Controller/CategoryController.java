8045e15227228f8d618b82d85a8b1a98c94b423b
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-02 13:24:17 +0200

    delete function + alert message

--- src/main/java/se/lexicon/springbootthymeleafproject/Controller/CategoryController.java
+++ src/main/java/se/lexicon/springbootthymeleafproject/Controller/CategoryController.java
@@ -4,6 +4,7 @@ package se.lexicon.springbootthymeleafproject.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import se.lexicon.springbootthymeleafproject.model.dto.CategoryView;
 
 import java.time.LocalDate;
@@ -46,6 +47,15 @@ public class CategoryController {
         model.addAttribute("categoryView", categoryView);
         return "category/category-view";
 
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteById(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
+        System.out.println("ID: " + id);
+        categoryViews.removeIf(categoryView1 -> categoryView1.getId() == id);
+        redirectAttributes.addFlashAttribute("message", "Category Id"+id+"is removed");
+        redirectAttributes.addFlashAttribute("alertClass", "alert alert-info");
+        return "redirect:/category/list";
     }
 
 
