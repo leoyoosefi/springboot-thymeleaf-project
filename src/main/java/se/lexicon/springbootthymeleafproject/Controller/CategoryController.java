542562968aa5158e5bcd3431254de2dc3429819a
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-02 19:43:51 +0200

    category form

--- src/main/java/se/lexicon/springbootthymeleafproject/Controller/CategoryController.java
+++ src/main/java/se/lexicon/springbootthymeleafproject/Controller/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import se.lexicon.springbootthymeleafproject.model.dto.CategoryForm;
 import se.lexicon.springbootthymeleafproject.model.dto.CategoryView;
 
 import java.time.LocalDate;
@@ -56,6 +57,21 @@ public class CategoryController {
         redirectAttributes.addFlashAttribute("message", "Category Id"+id+"is removed");
         redirectAttributes.addFlashAttribute("alertClass", "alert alert-info");
         return "redirect:/category/list";
+    }
+    @GetMapping("/form")
+    public String categoryForm(Model model){
+
+        CategoryForm categoryForm = new CategoryForm();
+        model.addAttribute("category",categoryForm );
+
+        return "category/category-form";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute("category") CategoryForm categoryForm){
+        System.out.println("categoryForm =" + categoryForm);
+        return "redirect:/category/list";
+
     }
 
 
---
+++ src/main/java/se/lexicon/springbootthymeleafproject/model/dto/CategoryForm.java
@@ -0,0 +1,14 @@
+package se.lexicon.springbootthymeleafproject.model.dto;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CategoryForm {
+
+    private String name;
+}
