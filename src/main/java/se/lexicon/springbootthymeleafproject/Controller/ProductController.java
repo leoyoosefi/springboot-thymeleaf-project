4a0dc0aea294ab8ca0889524fc3921653afdfb7e
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-04 11:33:15 +0200

    initiated product section

---
+++ src/main/java/se/lexicon/springbootthymeleafproject/Controller/ProductController.java
@@ -0,0 +1,22 @@
+package se.lexicon.springbootthymeleafproject.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/list")
+    public String showAllProduct(){
+        return "product/product-view";
+    }
+
+    @GetMapping("/form")
+    public String productForm(){
+        return "product/product-form";
+    }
+
+}
