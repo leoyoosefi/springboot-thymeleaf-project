8f18f8b2d5649c687ed571fa8176407dbc3f8248
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-08 19:15:51 +0200

    adding guest,user,admin login and logout function

--- src/main/java/se/lexicon/springbootthymeleafproject/security/SecurityConfiguration.java
+++ src/main/java/se/lexicon/springbootthymeleafproject/security/SecurityConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +20,13 @@ public class SecurityConfiguration {
         httpSecurity.formLogin();
 
         return httpSecurity.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails guest = User.withDefaultPasswordEncoder().username("guest").password("guest").roles("GUEST").build();
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("user").roles("GUEST","USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin").roles("GUEST","USER","ADMIN").build();
+        return new InMemoryUserDetailsManager(guest,user,admin);
     }
 
 }
